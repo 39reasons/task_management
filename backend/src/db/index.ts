5c18ac5081d98b3c83dfbff329e907ea018fdf5b
39reasons <128556474+39reasons@users.noreply.github.com>
2025-09-27 10:25:44 -0500

    .env and build change

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,11 +1,12 @@
 import { Pool, QueryResult, QueryResultRow } from "pg";
+import "dotenv/config";
 
 export const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  port: Number(process.env.DB_PORT),
 });
 
 export async function query<T extends QueryResultRow>(
